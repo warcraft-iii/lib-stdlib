679f15e356a02b7fa19050548a61cb08dfa94f15
Dencer <tdaddon@163.com>
2019-06-28 00:21:50 +0800

    feat: Add group.getUnits

--- oop/group.lua
+++ oop/group.lua
@@ -1,1 +1,13 @@
-return require('lib.stdlib.oop._generated._group')
+local Group = require('lib.stdlib.oop._generated._group')
+
+---getUnits
+---@return Unit[]
+function Group:getUnits()
+    local units = {}
+    self:forEach(function(unit)
+        table.insert(units, unit)
+    end)
+    return units
+end
+
+return Group
