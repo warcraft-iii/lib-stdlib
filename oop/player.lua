c56ba32e8b0f385af4d278a7b3d3460ff6bf8d81
Dencer <tdaddon@163.com>
2019-06-28 11:41:32 +0800

    feat: Add Player.iterateAll

--- oop/player.lua
+++ oop/player.lua
@@ -16,12 +16,28 @@ function Player:adjustState(state, delta)
     return self:setState(state, self:getState(state) + delta)
 end
 
+---adjustGold
+---@param delta integer
+---@return void
 function Player:adjustGold(delta)
     return self:adjustState(PlayerState.ResourceGold, delta)
 end
 
+---adjustLumber
+---@param delta integer
+---@return void
 function Player:adjustLumber(delta)
     return self:adjustState(PlayerState.ResourceLumber, delta)
+end
+
+---iterateAll
+---@return fun(): Player
+function Player:iterateAll()
+    return coroutine.wrap(function()
+        for i = 0, bj_MAX_PLAYER_SLOTS - 1 do
+            coroutine.yield(Player:get(i))
+        end
+    end)
 end
 
 return Player
