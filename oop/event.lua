3fc13b2841a0c60df8e8f0613f0f46ebd8edf934
Dencer <tdaddon@163.com>
2019-07-04 21:49:43 +0800

    refactor: Event.getTriggerRect

--- oop/event.lua
+++ oop/event.lua
@@ -1,11 +1,10 @@
 ---@type Event
 local Event = require('lib.stdlib.oop._generated._event')
-local Native = require('lib.stdlib.native.init')
 
 ---getTriggerRect
 ---@return Rect
 function Event:getTriggerRect()
-    return Trigger:fromUd(Native.GetTriggeringTrigger()).rect
+    return self:getTriggeringTrigger().rect
 end
 
 return Event
