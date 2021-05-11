c45947462cfde543333aa5514224f7de2c91d89a
Jai <814683@qq.com>
2021-05-11 17:55:49 +0800

    feat(classic): Impl FourCC function

--- native/init.lua
+++ native/init.lua
@@ -3,4 +3,18 @@
 -- @Link   : https://dengsir.github.io
 -- @Date   : 6/27/2019, 8:55:59 PM
 
+-- @classic@
+local bj = require('jass.globals')
+for index, value in pairs(bj) do
+    _G[index] = value
+end
+function FourCC(id)
+    if type(id) == 'number' then
+        return id
+    end
+    id = string.unpack('>I', id)
+    return id
+end
+-- @end-classic@
+
 return require('lib.stdlib.native.native')
